62494913aa0630240b56364ea6d5e51cdd536776
travkitch <124629790+travkitch@users.noreply.github.com>
2023-03-08 14:38:23 -0500

    Delete auth.py

--- auth.py
+++
@@ -1,8 +0,0 @@
-import tekore as tk
-
-
-def get_user_token():
-    CONFIG_FILE = 'credentials.config'
-    conf = tk.config_from_file(CONFIG_FILE, return_refresh=True)
-    token = tk.refresh_user_token(*conf[:2], conf[3])
-    return token
